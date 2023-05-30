ae8c58ffb8c123e1843f1337b6af3c57847fb5b2
Abdo <abdo@abdnh.net>
2023-05-31 02:30:12 +0300

    Persist visibility state

--- src/__init__.py
+++ src/__init__.py
@@ -3,12 +3,16 @@ from aqt.qt import *
 
 
 def toggle_menubar() -> None:
+    config = mw.addonManager.getConfig(__name__)
     if mw.form.menubar.isVisible():
         mw.form.menubar.hide()
     else:
         mw.form.menubar.show()
+    config["was_hidden"] = mw.form.menubar.isHidden()
+    mw.addonManager.writeConfig(__name__, config)
 
 
 config = mw.addonManager.getConfig(__name__)
 shortcut = QShortcut(config["shortcut"], mw)
 shortcut.activated.connect(toggle_menubar)
+mw.form.menubar.setVisible(not config["was_hidden"])
