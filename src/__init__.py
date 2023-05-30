1ff4cd71a44b5ba4f98e19cf5c668533e4aa3a02
Abdo <abdo@abdnh.net>
2023-05-31 02:37:08 +0300

    Use isVisible instead

--- src/__init__.py
+++ src/__init__.py
@@ -4,11 +4,8 @@ from aqt.qt import *
 
 def toggle_menubar() -> None:
     config = mw.addonManager.getConfig(__name__)
-    if mw.form.menubar.isVisible():
-        mw.form.menubar.hide()
-    else:
-        mw.form.menubar.show()
-    config["was_hidden"] = mw.form.menubar.isHidden()
+    mw.form.menubar.setVisible(not mw.form.menubar.isVisible())
+    config["was_hidden"] = not mw.form.menubar.isVisible()
     mw.addonManager.writeConfig(__name__, config)
 
 
