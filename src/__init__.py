e09b1408cdab1d047e7553d3cd7cd802fa8b6eec
Abdo <abdo@abdnh.net>
2024-02-22 05:31:15 +0300

    Fix shortcuts not working when menu bar is hidden

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,37 @@
-from aqt import mw
+from aqt import gui_hooks, mw
 from aqt.qt import *
+
+
+def hide_menubar() -> None:
+    mw.form.menubar.setFixedHeight(0)
+
+
+def show_menubar() -> None:
+    mw.form.menubar.setMaximumSize(QWIDGETSIZE_MAX, QWIDGETSIZE_MAX)
+    mw.form.menubar.setMinimumSize(0, 0)
 
 
 def toggle_menubar() -> None:
     config = mw.addonManager.getConfig(__name__)
-    mw.form.menubar.setVisible(not mw.form.menubar.isVisible())
-    config["was_hidden"] = not mw.form.menubar.isVisible()
+    is_hidden = not bool(mw.form.menubar.height())
+    if is_hidden:
+        show_menubar()
+    else:
+        hide_menubar()
+    config["was_hidden"] = not is_hidden
     mw.addonManager.writeConfig(__name__, config)
 
 
 config = mw.addonManager.getConfig(__name__)
 shortcut = QShortcut(config["shortcut"], mw)
 shortcut.activated.connect(toggle_menubar)
-mw.form.menubar.setVisible(not config["was_hidden"])
+
+
+def on_main_window_did_init():
+    if config["was_hidden"]:
+        hide_menubar()
+    else:
+        show_menubar()
+
+
+gui_hooks.main_window_did_init.append(on_main_window_did_init)
